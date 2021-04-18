dd7d9aa746a720c9e9c6b86b23983d4df81969ab
Raaed Kabir <raaed.kabir@gmail.com>
2021-04-18 21:08:29 +0800

    made paths relative

--- Testing.py
+++ Testing.py
@@ -1,8 +1,11 @@
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
 from PIL import Image
+
+path = os.path.dirname(__file__)
 
 i = 0
 coloumn = 0
@@ -15,12 +18,12 @@ ProperList = []
 abc = []
 xyz = []
 
-while i < len(glob.glob('C:\\Users\\Graheeth\\Desktop\\New folder\\Hole\\*')) :
+while i < len(glob.glob(f'{path}\\Hole\\*')) :
 
-    df = pd.read_csv('C:\\Users\\Graheeth\\Desktop\\New folder\\Hole\\FRD ' + str(i) +'.tas', delimiter = '\t', header=None)
+    df = pd.read_csv(f'{path}\\Hole\\FRD ' + str(i) +'.tas', delimiter = '\t', header=None)
    
     Horizontal = df.columns.values
-    Vertical = [*range(len(glob.glob('C:\\Users\\Graheeth\\Desktop\\New folder\\Hole\\*.tas')))]
+    Vertical = [*range(len(glob.glob(f'{path}\\Hole\\*.tas')))]
     
     HList = df.columns.values.tolist()
     TList = df.index.values.tolist()
@@ -33,17 +36,20 @@ while coloumn < len(Horizontal) :
     FRD = 0
 
 
-    while FRD < len(glob.glob('C:\\Users\\Graheeth\\Desktop\\New folder\\Hole\\*.tas')) :
+    while FRD < len(glob.glob(f'{path}\\Hole\\*.tas')) :
 
         FinalizedList = AList[FRD][coloumn]
         ProperList.append(FinalizedList)
 
-        if len(ProperList) == len(glob.glob('C:\\Users\\Graheeth\\Desktop\\New folder\\Hole\\*.tas')) :
+        if len(ProperList) == len(glob.glob(f'{path}\\Hole\\*.tas')) :
             abc.append(ProperList)
             ProperList = []
         FRD += 1
 
     coloumn += 1
+
+if not os.path.exists(f'{path}/images'):
+    os.makedirs(f'{path}/images')
 
 PicNum = 0
 
@@ -55,12 +61,12 @@ while PicNum < len(abc) :
     plt.colorbar()
     plt.axis('auto')
 
-    fig.savefig("C:\\Users\\Graheeth\\Desktop\\New folder\\images\\image " + str(PicNum) +".png")
+    fig.savefig(f'{path}\\images\\image {str(PicNum)}.png')
     
     PicNum += 1
 
 frames = []
-imgs = glob.glob("C:\\Users\\Graheeth\\Desktop\\New folder\\images\\*.png")
+imgs = glob.glob(f'{path}\\images\\*.png')
 
 imgs.sort(key=lambda y: int(y.split(" ")[-1][0:-4]))
 
@@ -69,9 +75,9 @@ for a in imgs:
     new_frame = Image.open(a)
     frames.append(new_frame)
 
-DurationCalc = round(len(glob.glob('C:\\Users\\Graheeth\\Desktop\\New folder\\images\\*.png')) / 30)
+DurationCalc = round(len(glob.glob(f'{path}\\images\\*.png')) / 30)
 
-frames[0].save('C:\\Users\\Graheeth\\Desktop\\New folder\\images\\gr.gif', format='GIF',
+frames[0].save(f'{path}\\images\\gr.gif', format='GIF',
             append_images=frames[1:],
             save_all=True,
             duration=DurationCalc, loop=0)
